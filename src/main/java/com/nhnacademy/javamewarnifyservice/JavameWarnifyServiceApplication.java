a7738c56166f3b1c131e7515ba2fdb2c17ae8861
nhnacademy <fhqht303@naver.com>
2025-04-21 15:31:49 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/javamewarnifyservice/JavameWarnifyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.javamewarnifyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavameWarnifyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavameWarnifyServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/javamewarnifyservice/JavameWarnifyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.javamewarnifyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavameWarnifyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
