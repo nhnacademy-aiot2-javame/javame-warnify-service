f1277284d76bd8d04b3c526fcda733d6e55c7bab
nhnacademy <fhqht303@naver.com>
2025-04-30 14:17:54 +0900

    feat-dooray: #11 DoorayService TEST Code 작성

---
+++ src/test/java/com/nhnacademy/javamewarnifyservice/service/impl/DoorayServiceTest.java
@@ -0,0 +1,185 @@
+package com.nhnacademy.javamewarnifyservice.service.impl;
+
+import com.nhnacademy.javamewarnifyservice.adaptor.CompanyAdaptor;
+import com.nhnacademy.javamewarnifyservice.dto.CompanyResponse;
+import feign.Request;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
+import org.springframework.web.client.RestClient;
+
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+
+@Slf4j
+@ActiveProfiles("dev")
+@SpringBootTest
+@AutoConfigureMockRestServiceServer
+class DoorayServiceTest {
+
+    @MockitoBean
+    CompanyAdaptor companyAdaptor;
+
+    @Autowired
+    DoorayService doorayService;
+
+    @Autowired
+    MockRestServiceServer serviceServer;
+
+    @Test
+    void sendAlarm() {
+        CompanyResponse companyResponse = new CompanyResponse(
+                "nhnacademy",
+                "엔에이엔",
+                "nhnacademy@naver.com",
+                "010-1111-1111",
+                "김해시 어딘가",
+                LocalDateTime.now(),
+                true
+        );
+
+        Mockito.when(companyAdaptor.getCompanyByDomain(Mockito.anyString())).thenReturn(new ResponseEntity<>(companyResponse,HttpStatus.OK));
+
+        // 멤버 조회 테스트
+        String email = "nhnacademy@naver.com";
+        String doorayURL = "https://api.dooray.com/common/v1/members?externalEmailAddresses=%s".formatted(email);
+
+        String result =
+                """
+                        {
+                            "header": {
+                                "resultCode": 0,
+                                "resultMessage": "",
+                                "isSuccessful": true
+                            },
+                            "result": [
+                                {
+                                    "id": "1",
+                                    "userCode": "user1",
+                                    "name": "john",
+                                    "externalEmailAddress": "user1@mail.com"
+                                }
+                            ],
+                            "totalCount": 1
+                        }
+                """;
+
+        serviceServer
+                .expect(requestTo(doorayURL))
+                .andExpect(MockRestRequestMatchers.method(HttpMethod.GET))
+                .andRespond(MockRestResponseCreators.withSuccess(result, MediaType.APPLICATION_JSON));
+
+        // 메세지 전송 테스트
+        String doorayURL1 = "https://api.dooray.com/messenger/v1/channels/direct-send";
+        String result1 = """
+                {
+                    "header": {
+                        "resultCode": 0,
+                        "resultMessage": "",
+                        "isSuccessful": true
+                    },
+                    "result": null
+                }
+                """;
+
+        serviceServer
+                .expect(requestTo(doorayURL1))
+                .andExpect(MockRestRequestMatchers.method(HttpMethod.POST))
+                .andRespond(MockRestResponseCreators.withSuccess(result1, MediaType.APPLICATION_JSON));
+
+        String sendResult = doorayService.sendAlarm("nhnacademy", "warninfo");
+
+        assertEquals("두레이 발신 성공", sendResult);
+    }
+
+    @Test
+    void getDoorayMemberIdTest() throws Exception{
+        String email = "fhqht303@naver.com";
+        String doorayURL = "https://api.dooray.com/common/v1/members?externalEmailAddresses=%s".formatted(email);
+
+        String result =
+                """
+                        {
+                            "header": {
+                                "resultCode": 0,
+                                "resultMessage": "",
+                                "isSuccessful": true
+                            },
+                            "result": [
+                                {
+                                    "id": "1",
+                                    "userCode": "user1",
+                                    "name": "john",
+                                    "externalEmailAddress": "user1@mail.com"
+                                }
+                            ],
+                            "totalCount": 1
+                        }
+                """;
+
+        serviceServer
+                .expect(requestTo(doorayURL))
+                .andExpect(MockRestRequestMatchers.method(HttpMethod.GET))
+                .andRespond(MockRestResponseCreators.withSuccess(result, MediaType.APPLICATION_JSON));
+
+        Method methoda = doorayService.getClass().getDeclaredMethod("getDoorayMemberId", String.class);
+        methoda.setAccessible(true);
+        String id = (String) methoda.invoke(doorayService, "fhqht303@naver.com");
+
+        log.info("id : {}",id);
+
+        assertNotNull(id);
+        assertEquals("1",id);
+    }
+
+    @Test
+    void sendIndividualMsgTest() throws Exception{
+        String doorayURL = "https://api.dooray.com/messenger/v1/channels/direct-send";
+        String result = """
+                {
+                    "header": {
+                        "resultCode": 0,
+                        "resultMessage": "",
+                        "isSuccessful": true
+                    },
+                    "result": null
+                }
+                """;
+
+        serviceServer
+                .expect(requestTo(doorayURL))
+                .andExpect(MockRestRequestMatchers.method(HttpMethod.POST))
+                .andRespond(MockRestResponseCreators.withSuccess(result, MediaType.APPLICATION_JSON));
+
+        Method method = doorayService.getClass().getDeclaredMethod("sendIndividualMsg", String.class, String.class);
+        method.setAccessible(true);
+
+        String id = (String) method.invoke(doorayService, "3884802321735904763", "Hello World");
+
+        log.info("id : {}", id);
+        assertNotNull(id);
+        assertEquals("true", id);
+    }
+
+}
